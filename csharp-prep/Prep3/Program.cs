0d4962b1416c8daff1eba3e6dd08ec714b16d0fb
ef10res <floreserick10@gmail.com>
2024-04-27 15:57:40 -0600

    Magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,101);
+
+        //Console.Write("What is your guess? ");
+        //int guess = int.Parse(Console.ReadLine());
+        int guess = 0;
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");        
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+        }   while (guess != magic);
+        if (guess == magic)
+        {
+            Console.WriteLine("You guessed it! ");
+        }
     }
 }
