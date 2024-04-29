a0968ccb173e9d73ae21af94a2a15a5aaa200698
ef10res <floreserick10@gmail.com>
2024-04-29 17:36:25 -0600

    Display Resume Class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job _job1 = new Job();
+        Job _job2 = new Job();
+
+        _job1._jobTitle = "Software Engineer";
+        _job1._company = "Microsoft";
+        _job1._startYear = 2024;
+        _job1._endYear = 2030;
+
+        _job2._jobTitle = "Manager";
+        _job2._company = "Apple";
+        _job2._startYear = 2030;
+        _job2._endYear = 2040;
+
+        //_job1.Display();
+        //_job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Erick Flores";
+        myResume._jobs.Add(_job1);
+        myResume._jobs.Add(_job2);
+
+        myResume.Display();
     }
 }
