305ca2510649152c862e91895424575926e8ba59
ef10res <floreserick10@gmail.com>
2024-05-08 07:23:17 -0600

    Fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,57 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string _stringFraction = $"{_top}/{_bottom}";
+
+        return _stringFraction;
+    }
+
+    public double GetDecimalValue()
+    {
+        double _decimal = (double)_top / (double)_bottom;
+
+        return _decimal;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,22 @@
-using System;
 
-class Program
+class ProgramFraction
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction _fraction = new Fraction();
+        Console.WriteLine($"{_fraction.GetFractionString()}");
+        Console.WriteLine($"{_fraction.GetDecimalValue()}");
+
+        Fraction _fraction1 = new Fraction(5);
+        Console.WriteLine(_fraction1.GetFractionString());
+        Console.WriteLine(_fraction1.GetDecimalValue());
+
+        Fraction _fraction2 = new Fraction(3,4);
+        Console.WriteLine(_fraction2.GetFractionString());
+        Console.WriteLine(_fraction2.GetDecimalValue());
+
+        Fraction _fraction3 = new Fraction(1,3);
+        Console.WriteLine(_fraction3.GetFractionString());
+        Console.WriteLine(_fraction3.GetDecimalValue());
     }
 }
