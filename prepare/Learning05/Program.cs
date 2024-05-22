c502bb75b57e8ca6a863380f82c3b20e211cb33a
ef10res <floreserick10@gmail.com>
2024-05-22 06:04:06 -0600

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("yellow", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("red", 4, 6);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("blue", 4);
+        shapes.Add(circle);
+
+        //Console.WriteLine($"The square is {square.GetColor()} and the area is {square.GetArea()}");
+        //Console.WriteLine($"The rectangle is {rectangle.GetColor()} and the area is {rectangle.GetArea()}");
+        //Console.WriteLine($"The squre is {circle.GetColor()} and the area is {circle.GetArea()}");
+        
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,27 @@
+using System.Drawing;
+using System.Formats.Asn1;
+
+public class Shape
+{
+    protected string _color;
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
